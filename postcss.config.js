f05a9631f8a7103bdc2abe5ad571929bcab833a7
Justin Shaw <realjustinshaw@gmail.com>
2022-01-16 00:00:07 -0800

    patch postcss config bug

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
 	plugins: [
-		require('postcss-import'),
-		require('tailwindcss/nesting'),
-		require('tailwindcss'),
-		require('autoprefixer'),
+		'postcss-import',
+		'tailwindcss/nesting',
+		'tailwindcss',
+		'autoprefixer',
 	],
 };
